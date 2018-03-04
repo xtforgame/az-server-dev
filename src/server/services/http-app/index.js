c33c010ff28dbe5fa3d89b55781436ceee6dd80d
RickChen <xtforgame@gmail.com>
2018-03-04 13:13:09 +0800

    Add az-restful-helpers, remove bottlejs

--- src/server/services/http-app/index.js
+++ src/server/services/http-app/index.js
@@ -4,6 +4,7 @@ import Koa from 'koa';
 import createRouterClass from 'generic-router';
 import bodyParser from 'koa-bodyparser';
 import { runServer } from './http-server';
+import { RestfulError } from 'az-restful-helpers';
 import http from 'http';
 
 let methods = http.METHODS.map(function lowerCaseMethod (method) {
@@ -21,10 +22,14 @@ export default class HttpApp extends ServiceBase {
     // prevent any error to be sent to user
     this.app.use((ctx, next) => {
       return next().catch((err) => {
+        if(err instanceof RestfulError){
+          return err.koaThrow(ctx);
+        }
+        // console.log('err.restfulError :', err.restfulError);
         if(!err.status){
           console.error(err);
           console.error(err.stack);
-          ctx.throw(JSON.stringify({error: 'Internal Server Error'}), 500);
+          ctx.throw(500);
         }
         throw err;
       });
